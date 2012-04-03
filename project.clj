e997cb538aa5e6992fe6635b34ee3a260768305b
jaycfields <github@jayfields.com>
2012-04-03 14:34:05 -0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-publishers "1.0.5"
+(defproject lein-publishers "1.0.6"
   :description "publish to various repos using lein"
   :eval-in-leiningen true
   :jar-name lein-publishers.jar
