cf671d6d368cc434218ef53535dd31d7b00c5d5a
Jay Fields <jay@jayfields.com>
2013-05-15 12:33:22 -0400

    update publish-fig

--- src/leiningen/publish_fig.clj
+++ src/leiningen/publish_fig.clj
@@ -1,16 +1,14 @@
-(ns leiningen.publish-fig
-  (:require leiningen.jar leiningen.clean clojure.java.io)
+(ns leiningen.tasks.publish-fig
+  (:require clojure.java.io)
   (:use clojure.java.shell))
 
 (defn publish-fig [project & args]
-  (leiningen.clean/clean project)
-  (leiningen.jar/jar project)
   (when-not (.exists (clojure.java.io/file "package.fig"))
     (spit "package.fig" (str "resource \"" (:jar-name project) "\"\n\nconfig default\n"
                              "  append CLASSPATH=@/" (:jar-name project) "\n"
                              "  append SOURCEPATH=@/" (:jar-name project) "\n"
-                             "end"))
-    )
+                             "end")))
+  (println "sh" "fig" "--publish" (str (:name project) "/" (:version project)) args)
   (let [response (apply sh "fig" "--publish" (str (:name project) "/" (:version project)) args)]
     (println "OUT:" (:out response))
     (println "ERR:" (:err response))))
