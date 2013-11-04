1c769ed00f08e73d6f423bedf848afb3b864dd8a
Jay Fields <jay@jayfields.com>
2013-11-04 11:34:23 -0500

    append target to the default package.fig file

--- src/leiningen/publish_fig.clj
+++ src/leiningen/publish_fig.clj
@@ -4,9 +4,9 @@
 
 (defn publish-fig [project & args]
   (when-not (.exists (clojure.java.io/file "package.fig"))
-    (spit "package.fig" (str "resource \"" (:jar-name project) "\"\n\nconfig default\n"
-                             "  append CLASSPATH=@/" (:jar-name project) "\n"
-                             "  append SOURCEPATH=@/" (:jar-name project) "\n"
+    (spit "package.fig" (str "resource \"target/" (:jar-name project) "\"\n\nconfig default\n"
+                             "  append CLASSPATH=@/target/" (:jar-name project) "\n"
+                             "  append SOURCEPATH=@/target/" (:jar-name project) "\n"
                              "end")))
   (apply println "sh" "fig" "--publish" (str (:name project) "/" (:version project)) args)
   (let [response (apply sh "fig" "--publish" (str (:name project) "/" (:version project)) args)]
