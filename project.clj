2f3ceff6eaaef4d882111950b60bf446b597cbff
Jay Fields <github@jayfields.com>
2014-09-24 12:26:00 -0500

    stuff

--- project.clj
+++ project.clj
@@ -1,5 +1,15 @@
-(defproject lein-publishers "1.0.11"
+(defproject lein-publishers "1.0.12"
   :description "publish to fig using lein"
   :eval-in-leiningen true
   :source-paths ["src"]
-  :jar-name "lein-publishers.jar")
+  :jar-name "lein-publishers.jar"
+  :release-tasks [["clean"]
+                  ["check"]
+                  ["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ;; ["vcs" "tag"] would like to have this, but it requires we all set up gpg keys to sign tag objects.
+                  ["jar"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
